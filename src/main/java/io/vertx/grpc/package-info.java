4fc20ed9d3cec2f6b9445db3b27ae8631bcdb885
Julien Viet <julien@julienviet.com>
2016-12-12 14:03:00 +0100

    Redo the proof of concept reusing the Netty bits and integrating them with Vert.x event loop

---
+++ src/main/java/io/vertx/grpc/package-info.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * - worker integration
+ * - SSL integration
+ * - options etc...
+ * - bidi streaming
+ * - multi server binding
+ *
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+package io.vertx.grpc;
