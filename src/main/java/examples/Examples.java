084d906e1335ebfee0e805a43a4bda33c1f37403
Julien Viet <julien@julienviet.com>
2016-12-13 09:57:23 +0100

    More doc

---
+++ src/main/java/examples/Examples.java
@@ -0,0 +1,85 @@
+package examples;
+
+import io.grpc.ManagedChannel;
+import io.grpc.stub.StreamObserver;
+import io.vertx.core.Vertx;
+import io.vertx.core.net.JksOptions;
+import io.vertx.docgen.Source;
+import io.vertx.grpc.StreamHelper;
+import io.vertx.grpc.VertxChannelBuilder;
+import io.vertx.grpc.VertxServer;
+import io.vertx.grpc.VertxServerBuilder;
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+@Source
+public class Examples {
+
+  public void simpleServer(Vertx vertx) throws Exception {
+
+    // The rcp service
+    GreeterGrpc.GreeterImplBase service = new GreeterGrpc.GreeterImplBase() {
+      @Override
+      public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        responseObserver.onNext(HelloReply.newBuilder().setMessage(request.getName()).build());
+        responseObserver.onCompleted();
+      }
+    };
+
+    // Create the server
+    VertxServer rpcServer = VertxServerBuilder
+        .forAddress(vertx, "my.host", 8080)
+        .addService(service)
+        .build();
+
+    // Start is asynchronous
+    rpcServer.start();
+  }
+
+  public void simpleClient(Vertx vertx) {
+
+    // Create the channel
+    ManagedChannel channel = VertxChannelBuilder
+        .forAddress(vertx, "localhost", 8080)
+        .usePlaintext(true)
+        .build();
+
+    // Get a stub to use for interacting with the remote service
+    GreeterGrpc.GreeterStub stub = GreeterGrpc.newStub(channel);
+
+    // Make a request
+    HelloRequest request = HelloRequest.newBuilder().setName("Julien").build();
+
+    // Call the remote service
+    stub.sayHello(request, StreamHelper.future(ar -> {
+      if (ar.succeeded()) {
+        System.out.println("Got the server response: " + ar.result().getMessage());
+      } else {
+        System.out.println("Coult not reach server " + ar.cause().getMessage());
+      }
+    }));
+  }
+
+  public void sslServer(Vertx vertx) {
+    VertxServerBuilder builder = VertxServerBuilder.forPort(vertx, 8080)
+        .useSsl(options -> options
+            .setSsl(true)
+            .setUseAlpn(true)
+            .setKeyStoreOptions(new JksOptions()
+                .setPath("server-keystore.jks")
+                .setPassword("secret")));
+  }
+
+  public void sslClient(Vertx vertx) {
+    ManagedChannel channel = VertxChannelBuilder.
+        forAddress(vertx, "localhost", 8080)
+        .useSsl(options -> options
+            .setSsl(true)
+            .setUseAlpn(true)
+            .setTrustStoreOptions(new JksOptions()
+                .setPath("client-truststore.jks")
+                .setPassword("secret")))
+        .build();
+  }
+}
--- src/main/java/io/vertx/grpc/package-info.java
+++ src/main/java/io/vertx/grpc/package-info.java
@@ -1,6 +1,36 @@
 /**
- * Write doc.
+ * = Vert.x gRPC
  *
- * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ * == gRPC Server
+ *
+ * [source,$lang]
+ * ----
+ * {@link examples.Examples#simpleServer}
+ * ----
+ *
+ * SSL configuration
+ *
+ * [source,$lang]
+ * ----
+ * {@link examples.Examples#sslServer}
+ * ----
+ *
+ * == gRPC Client
+ *
+ * [source,$lang]
+ * ----
+ * {@link examples.Examples#simpleClient}
+ * ----
+ *
+ * SSL configuration
+ *
+ * [source,$lang]
+ * ----
+ * {@link examples.Examples#sslClient}
+ * ----
+ *
  */
+@Document(fileName = "index.adoc")
 package io.vertx.grpc;
+
+import io.vertx.docgen.Document;
