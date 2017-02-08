6a8bfbde9690d513a53d46e04ea53fe1d9413e65
Paulo Lopes <paulo@mlopes.net>
2017-02-08 10:24:10 +0100

    returs now use streams

---
+++ src/main/java/io/vertx/grpc/GrpcReadStream.java
@@ -0,0 +1,70 @@
+package io.vertx.grpc;
+
+import io.grpc.stub.StreamObserver;
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.codegen.annotations.GenIgnore;
+import io.vertx.codegen.annotations.Nullable;
+import io.vertx.codegen.annotations.VertxGen;
+import io.vertx.core.Handler;
+import io.vertx.core.streams.ReadStream;
+import io.vertx.grpc.impl.GrpcReadStreamImpl;
+
+@VertxGen
+public interface GrpcReadStream<T> extends ReadStream<T> {
+
+    static <T> GrpcReadStream<T> create(StreamObserver<T> observer) {
+        return new GrpcReadStreamImpl<T>(observer);
+    }
+
+    static <T> GrpcReadStream<T> create() {
+        return new GrpcReadStreamImpl<T>();
+    }
+
+    /**
+     * Set an exception handler on the read stream.
+     *
+     * @param handler  the exception handler
+     * @return a reference to this, so the API can be used fluently
+     */
+    GrpcReadStream<T> exceptionHandler(Handler<Throwable> handler);
+
+    /**
+     * Set a data handler. As data is read, the handler will be called with the data.
+     *
+     * @return a reference to this, so the API can be used fluently
+     */
+    @Fluent
+    GrpcReadStream<T> handler(@Nullable Handler<T> handler);
+
+    /**
+     * Pause the {@code ReadSupport}. While it's paused, no data will be sent to the {@code dataHandler}
+     *
+     * @return a reference to this, so the API can be used fluently
+     */
+    @Fluent
+    GrpcReadStream<T> pause();
+
+    /**
+     * Resume reading. If the {@code ReadSupport} has been paused, reading will recommence on it.
+     *
+     * @return a reference to this, so the API can be used fluently
+     */
+    @Fluent
+    GrpcReadStream<T> resume();
+
+    /**
+     * Set an end handler. Once the stream has ended, and there is no more data to be read, this handler will be called.
+     *
+     * @return a reference to this, so the API can be used fluently
+     */
+    @Fluent
+    GrpcReadStream<T> endHandler(@Nullable Handler<Void> endHandler);
+
+    /**
+     * Should not be used by end user, it is a simple accessor the the underlying gRPC StreamObserver.
+     *
+     * @return the underlying stream observer.
+     */
+    @GenIgnore
+    StreamObserver<T> observer();
+}
