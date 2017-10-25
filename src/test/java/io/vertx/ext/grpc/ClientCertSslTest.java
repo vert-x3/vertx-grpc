966f4a4cce660bdcf3bd7c85dc205480321fddde
Andreas Mack <andreas.mack@konsec.com>
2017-10-25 17:50:49 +0200

    Add netty code.

--- src/test/java/io/vertx/ext/grpc/ClientCertSslTest.java
+++ src/test/java/io/vertx/ext/grpc/ClientCertSslTest.java
@@ -5,6 +5,13 @@ import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
 
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.NettyServerBuilder;
+import io.grpc.stub.StreamObserver;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 
@@ -19,13 +26,19 @@ import io.vertx.grpc.VertxChannelBuilder;
 import io.vertx.grpc.VertxServerBuilder;
 import org.junit.Test;
 
+import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLPeerUnverifiedException;
 import javax.net.ssl.SSLSession;
 import javax.security.auth.x500.X500Principal;
+import java.io.File;
+import java.io.IOException;
 import java.security.Principal;
 import java.security.cert.Certificate;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static junit.framework.TestCase.assertEquals;
 
 
 public class ClientCertSslTest extends GrpcTestBase  {
@@ -43,39 +56,7 @@ public class ClientCertSslTest extends GrpcTestBase  {
         future.complete(HelloReply.newBuilder().setMessage("Hello " + req.getName()).build());
       }
     };
-    ServerServiceDefinition sd = ServerInterceptors.intercept(service,
-      new ServerInterceptor() {
-
-        @Override
-        public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata,
-                                                                     ServerCallHandler<ReqT, RespT> serverCallHandler) {
-          System.out.println("We're in");
-          SSLSession sslSession = serverCall.getAttributes().get(Grpc.TRANSPORT_ATTR_SSL_SESSION);
-          if (sslSession == null) {
-            throw new IllegalArgumentException("No ssl session");
-          }
-
-          String cn = "";
-          try {
-            Certificate[] certs = sslSession.getPeerCertificates();
-            System.out.println("Certs:"+certs.length);
-            Principal principal = sslSession.getPeerPrincipal();
-            if (!(principal instanceof X500Principal)) {
-              throw new IllegalArgumentException("Not authenticated");
-            }
-            X500Principal x509principal = (X500Principal)principal;
-            Pattern p = Pattern.compile("(^|,)CN=([^,]*)(,|$)");
-            Matcher matcher = p.matcher(x509principal.getName());
-            if(matcher.find()) cn = matcher.group(2);
-            System.out.println("CN:"+cn);
-          } catch (SSLPeerUnverifiedException e) {
-            System.out.println("Peer is not verified:"+e.getMessage());
-            throw new IllegalArgumentException("Peer is not verified:"+e.getMessage());
-          }
-          return Contexts.interceptCall(
-            Context.current().withValue(SSL_COMMON_NAME, cn), serverCall, metadata, serverCallHandler);
-        }
-      });
+    ServerServiceDefinition sd = getServerServiceDefinition(service);
 
     serverCtx.runOnContext(v -> startServer(sd, VertxServerBuilder.forPort(vertx,port)
         .useSsl(options -> {
@@ -132,5 +113,89 @@ public class ClientCertSslTest extends GrpcTestBase  {
 
   }
 
+  private static ServerServiceDefinition getServerServiceDefinition(BindableService service) {
+    return ServerInterceptors.intercept(service,
+      new ServerInterceptor() {
+
+        @Override
+        public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata,
+                                                                     ServerCallHandler<ReqT, RespT> serverCallHandler) {
+          System.out.println("We're in");
+          SSLSession sslSession = serverCall.getAttributes().get(Grpc.TRANSPORT_ATTR_SSL_SESSION);
+          if (sslSession == null) {
+            throw new IllegalArgumentException("No ssl session");
+          }
+
+          String cn = "";
+          try {
+            Certificate[] certs = sslSession.getPeerCertificates();
+            System.out.println("Certs:"+certs.length);
+            Principal principal = sslSession.getPeerPrincipal();
+            if (!(principal instanceof X500Principal)) {
+              throw new IllegalArgumentException("Not authenticated");
+            }
+            X500Principal x509principal = (X500Principal)principal;
+            Pattern p = Pattern.compile("(^|,)CN=([^,]*)(,|$)");
+            Matcher matcher = p.matcher(x509principal.getName());
+            if(matcher.find()) cn = matcher.group(2);
+            System.out.println("CN:"+cn);
+          } catch (SSLPeerUnverifiedException e) {
+            System.out.println("Peer is not verified:"+e.getMessage());
+            throw new IllegalArgumentException("Peer is not verified:"+e.getMessage());
+          }
+          return Contexts.interceptCall(
+            Context.current().withValue(SSL_COMMON_NAME, cn), serverCall, metadata, serverCallHandler);
+        }
+      });
+  }
+
+  @Test
+  public void nettyPass(TestContext ctx) throws IOException, InterruptedException {
+    ManagedChannelBuilder builder = NettyChannelBuilder
+      .forAddress("localhost", port)
+      .sslContext(GrpcSslContexts.forClient()
+        .startTls(true)
+        //.clientAuth(ClientAuth.REQUIRE)
+
+        .trustManager(new File("src/test/resources/tls/TestCA.crt"))
+        .keyManager(new File("src/test/resources/tls/TestClient.crt"),
+          new File("src/test/resources/tls/TestClient.p8"))
+        .build());
+    ManagedChannel channel = builder.build();
+    GreeterGrpc.GreeterBlockingStub stub  = GreeterGrpc.newBlockingStub(channel);
+    //Async started = ctx.async();
+    //io.vertx.core.Context serverCtx = vertx.getOrCreateContext();
+    BindableService service = new GreeterGrpc.GreeterImplBase() {
+      @Override
+      public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        responseObserver.onNext(HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
+        responseObserver.onCompleted();
+      }
+    };
+    ServerServiceDefinition sslSd = getServerServiceDefinition(service);
+  SslProvider sslProvider = SslContext.defaultServerProvider();
+  SslContextBuilder contextBuilder = SslContextBuilder.forServer(new File("src/test/resources/tls/TestServerChain.pem"),
+    new File("src/test/resources/tls/TestServer.p8"));
+  GrpcSslContexts.configure(contextBuilder, sslProvider);
+
+  SslContext sslContext = contextBuilder
+    .startTls(true)
+    .trustManager(new File("src/test/resources/tls/TestCA.crt"))
+    .clientAuth(io.netty.handler.ssl.ClientAuth.REQUIRE).build();
+   Server ssl_server = NettyServerBuilder.forPort(port).sslContext(sslContext)
+
+    .addService(sslSd)
+    .build();
+
+  ssl_server.start();
+
+
+    HelloRequest request = HelloRequest.newBuilder().setName("Julien").build();
+    HelloReply reply = stub.sayHello(request);
+    System.out.println("Reply:" + reply.getMessage());
+    ctx.assertEquals("Hello Julien", reply.getMessage());
+    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    ssl_server.shutdownNow().awaitTermination(5,TimeUnit.SECONDS);
+}
 
 }
