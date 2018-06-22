475f9c13d5f3f0631814f553e25fa8a9143f0376
Josef Pfleger <green-carrot@orange-cactus.org>
2018-06-22 10:53:37 +0200

    add assume to NativeTransportTest
    Signed-off-by: Josef Pfleger <green-carrot@orange-cactus.org>

--- src/test/java/io/vertx/ext/grpc/NativeTransportTest.java
+++ src/test/java/io/vertx/ext/grpc/NativeTransportTest.java
@@ -3,9 +3,11 @@ package io.vertx.ext.grpc;
 import examples.GreeterGrpc;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.net.impl.transport.Transport;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.grpc.VertxServerBuilder;
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,11 +16,13 @@ public class NativeTransportTest {
 
   @Test
   public void testNativeTransportEnabled(TestContext ctx) {
+    assumeNativeTransport();
     testInternal(ctx, Vertx.vertx(new VertxOptions().setPreferNativeTransport(true)));
   }
 
   @Test
   public void testNativeTransportDisabled(TestContext ctx) {
+    assumeNativeTransport();
     testInternal(ctx, Vertx.vertx(new VertxOptions().setPreferNativeTransport(false)));
   }
 
@@ -27,5 +31,10 @@ public class NativeTransportTest {
       .addService(new GreeterGrpc.GreeterImplBase() { })
       .build()
       .start(ctx.asyncAssertSuccess());
+  }
+
+  private void assumeNativeTransport() {
+    Transport nativeTransport = Transport.nativeTransport();
+    Assume.assumeTrue(nativeTransport != null && nativeTransport.isAvailable());
   }
 }
