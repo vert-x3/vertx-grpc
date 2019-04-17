2ee7704361eecbdbbe1a6b5f358b87b64d3d6904
Paulo Lopes <pmlopes@gmail.com>
2019-04-17 11:25:55 +0200

    Upgrade GRPC to 1.20.0 and netty to 4.1.34

--- src/main/java/examples/GreeterGrpc.java
+++ src/main/java/examples/GreeterGrpc.java
@@ -21,7 +21,7 @@ import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.15.0)",
+    value = "by gRPC proto compiler (version 1.20.0)",
     comments = "Source: helloworld.proto")
 public final class GreeterGrpc {
 
