701d83ab79f71cee433b4fe5cd22feead9570bc2
indiketa <eduard.catala@gmail.com>
2019-08-21 11:31:30 +0200

    Modify ClientCalls to use WriteStream

--- vertx-grpc/src/main/java/io/vertx/grpc/stub/ClientCalls.java
+++ vertx-grpc/src/main/java/io/vertx/grpc/stub/ClientCalls.java
@@ -2,78 +2,53 @@ package io.vertx.grpc.stub;
 
 import io.grpc.stub.StreamObserver;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.Promise;
 import io.vertx.core.streams.ReadStream;
+import io.vertx.core.streams.WriteStream;
 
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 /**
  * @author Rogelio Orts
+ * @author Eduard Català
  */
 public final class ClientCalls {
 
   private ClientCalls() {
   }
 
-  public static <TRequest, TResponse> Future<TResponse> oneToOne(
-      Future<TRequest> futureSource,
-      BiConsumer<TRequest, StreamObserver<TResponse>> delegate) {
-    Promise<TResponse> promise = Promise.promise();
-
-    futureSource.setHandler(res -> {
-      if (res.succeeded()) {
-        delegate.accept(res.result(), receiveOne(promise));
-      } else {
-        promise.fail(res.cause());
-      }
-    });
-
+  public static <I, O> Future<O> oneToOne(I request, BiConsumer<I, StreamObserver<O>> delegate) {
+    Promise<O> promise = Promise.promise();
+    delegate.accept(request, toStreamObserver(promise));
     return promise.future();
   }
 
-  public static <TRequest, TResponse> ReadStream<TResponse> oneToMany(
-      Future<TRequest> futureSource,
-      BiConsumer<TRequest, StreamObserver<TResponse>> delegate) {
-    StreamObserverReadStream<TResponse> readStream = new StreamObserverReadStream<>();
-
-    futureSource.setHandler(res -> {
-      if (res.succeeded()) {
-        delegate.accept(res.result(), readStream);
-      } else {
-        readStream.onError(res.cause());
-      }
-    });
-
-    return readStream;
+  public static <I, O> ReadStream<O> oneToMany(I request, BiConsumer<I, StreamObserver<O>> delegate) {
+    StreamObserverReadStream<O> response = new StreamObserverReadStream<>();
+    delegate.accept(request, response);
+    return response;
   }
 
-  public static <TRequest, TResponse> Future<TResponse> manyToOne(
-      ReadStream<TRequest> readStreamSource,
-      Function<StreamObserver<TResponse>, StreamObserver<TRequest>> delegate) {
-    readStreamSource.pause();
-    Promise<TResponse> promise = Promise.promise();
-    StreamObserver<TRequest> streamObserverRequest = delegate.apply(receiveOne(promise));
-    receiveMany(readStreamSource, streamObserverRequest);
-
+  public static <I, O> Future<O> manyToOne(Handler<WriteStream<I>> requestHandler, Function<StreamObserver<O>, StreamObserver<I>> delegate) {
+    Promise<O> promise = Promise.promise();
+    StreamObserver<I> request = delegate.apply(toStreamObserver(promise));
+    requestHandler.handle(new GrpcWriteStream(request));
     return promise.future();
   }
 
-  public static <TRequest, TResponse> ReadStream<TResponse> manyToMany(
-      ReadStream<TRequest> readStreamSource,
-      Function<StreamObserver<TResponse>, StreamObserver<TRequest>> delegate) {
-    readStreamSource.pause();
-    StreamObserverReadStream<TResponse> streamObserverResponse = new StreamObserverReadStream<>();
-    StreamObserver<TRequest> streamObserverRequest = delegate.apply(streamObserverResponse);
-    receiveMany(readStreamSource, streamObserverRequest);
-
-    return streamObserverResponse;
+  public static <I, O> ReadStream<O> manyToMany(Handler<WriteStream<I>> requestHandler, Function<StreamObserver<O>, StreamObserver<I>> delegate) {
+    StreamObserverReadStream<O> response = new StreamObserverReadStream<>();
+    StreamObserver<I> request = delegate.apply(response);
+    requestHandler.handle(new GrpcWriteStream(request));
+    return response;
   }
 
-  private static <TResponse> StreamObserver<TResponse> receiveOne(Promise<TResponse> promise) {
-    return new StreamObserver<TResponse>() {
+  private static <O> StreamObserver<O> toStreamObserver(Promise<O> promise) {
+    return new StreamObserver<O>() {
       @Override
-      public void onNext(TResponse tResponse) {
+      public void onNext(O tResponse) {
         promise.complete(tResponse);
       }
 
@@ -87,13 +62,6 @@ public final class ClientCalls {
         // Do nothing
       }
     };
-  }
-
-  private static <TRequest> void receiveMany(ReadStream<TRequest> readStreamSource, StreamObserver<TRequest> streamObserverRequest) {
-    readStreamSource.endHandler(v -> streamObserverRequest.onCompleted());
-    readStreamSource.exceptionHandler(streamObserverRequest::onError);
-    readStreamSource.handler(streamObserverRequest::onNext);
-    readStreamSource.resume();
   }
 
 }
