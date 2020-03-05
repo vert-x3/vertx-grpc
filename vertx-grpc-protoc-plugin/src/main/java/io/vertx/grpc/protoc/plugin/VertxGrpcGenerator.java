f5eb92cec3a3e472b8eba3cf7e0e2c3346de44fe
Paulo Lopes <pmlopes@gmail.com>
2020-03-05 13:41:44 +0100

    cannot use system properties as they are not passed to the compiler. Must use env vars.
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- vertx-grpc-protoc-plugin/src/main/java/io/vertx/grpc/protoc/plugin/VertxGrpcGenerator.java
+++ vertx-grpc-protoc-plugin/src/main/java/io/vertx/grpc/protoc/plugin/VertxGrpcGenerator.java
@@ -22,8 +22,8 @@ public class VertxGrpcGenerator extends Generator {
   private static final int SERVICE_NUMBER_OF_PATHS = 2;
   private static final int METHOD_NUMBER_OF_PATHS = 4;
 
-  private static final String CLASS_PREFIX = System.getProperty("vertx.grpc.class.prefix", "Vertx");
-  private static final String GRPC_TEMPLATE_PATH = System.getProperty("vertx.grpc.template.path", "VertxStub.mustache");
+  private static final String CLASS_PREFIX = System.getenv().getOrDefault("VERTX_GRPC_CLASSPREFIX", "Vertx").trim();
+  private static final String GRPC_TEMPLATE_PATH = System.getenv().getOrDefault("VERTX_GRPC_TEMPLATE", "VertxStub.mustache").trim();
 
   private String getServiceJavaDocPrefix() {
     return "    ";
