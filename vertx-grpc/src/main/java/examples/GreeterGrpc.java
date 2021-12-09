6582fe33ae9efa33895e556a5253b4c5b74bcefa
Julien Viet <julien@julienviet.com>
2021-12-09 16:20:15 +0100

    Upgrade grpc-java to 1.42.1 - fixes #107

--- vertx-grpc/src/main/java/examples/GreeterGrpc.java
+++ vertx-grpc/src/main/java/examples/GreeterGrpc.java
@@ -8,8 +8,9 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.38.1)",
+    value = "by gRPC proto compiler (version 1.42.1)",
     comments = "Source: helloworld.proto")
+@io.grpc.stub.annotations.GrpcGenerated
 public final class GreeterGrpc {
 
   private GreeterGrpc() {}
