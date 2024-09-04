b8ce7ee93c8f79c8b627997c16d7ba9845f8ddae
Julien Viet <julien@julienviet.com>
2024-09-04 15:58:35 +0200

    Set version to 4.5.11-SNAPSHOT

--- vertx-grpc/src/main/java/examples/GreeterGrpc.java
+++ vertx-grpc/src/main/java/examples/GreeterGrpc.java
@@ -8,7 +8,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  * </pre>
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.61.0)",
+    value = "by gRPC proto compiler (version 1.65.0)",
     comments = "Source: helloworld.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class GreeterGrpc {
